8d6cf9fc1476003d1ccb1e7b23183d963ffec6c1
jr-philip <philipmwaura48@gmail.com>
2023-01-21 22:39:03 -0600

    radio

--- index.js
+++ index.js
@@ -15,7 +15,88 @@ function renderEmotionsRadios(cats){
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
-        radioItems += `<p>${emotion}</p>`
+        radioItems += `<div class="radio">
+        <input 
+        type="radio"
+        id="emotion"
+        value="emotion
+        name="cat emotion
+        >
+        <label for="emotion">moody</label>
+        </div>
+        <div class="radio">
+        <input 
+        type="radio"
+        id="emotion"
+        value="emotion
+        name="cat emotion
+        >
+        <label for="emotion">insomniac</label>
+        </div>
+          <div class="radio">
+        <input 
+        type="radio"
+        id="emotion"
+        value="emotion
+        name="cat emotion
+        >
+        <label for="emotion">confused</label>
+        </div>
+          <div class="radio">
+        <input 
+        type="radio"
+        id="emotion"
+        value="emotion
+        name="cat emotion
+        >
+        <label for="emotion">sad</label>
+        </div>
+          <div class="radio">
+        <input 
+        type="radio"
+        id="emotion"
+        value="emotion
+        name="cat emotion
+        >
+        <label for="emotion">dominant</label>
+        </div>
+          <div class="radio">
+        <input 
+        type="radio"
+        id="emotion"
+        value="emotion
+        name="cat emotion
+        >
+        <label for="emotion">happy</label>
+        </div>
+          <div class="radio">
+        <input 
+        type="radio"
+        id="emotion"
+        value="emotion
+        name="cat emotion
+        >
+        <label for="emotion">relaxed</label>
+        </div>
+          <div class="radio">
+        <input 
+        type="radio"
+        id="emotion"
+        value="emotion
+        name="cat emotion
+        >
+        <label for="emotion">hungry</label>
+        </div>
+          <div class="radio">
+        <input 
+        type="radio"
+        id="emotion"
+        value="emotion
+        name="cat emotion
+        >
+        <label for="emotion">scared</label>
+        </div>
+        `
     }
     emotionRadios.innerHTML = radioItems
 }
