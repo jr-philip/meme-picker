ad23d2959b46e415f39d50f6e18e9d1e33d7e975
jr-philip <philipmwaura48@gmail.com>
2023-01-24 21:14:27 -0600

    use of e.target.id

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import { catsData } from "./data.js"
 const emotionRadios = document.getElementById("emotion-radios")
 
+emotionRadios.addEventListener("change",function(e){
+    console.log(e.target.id)// use of e.target.id 
+})
+
 function getEmotionsArray(cats) {
     const catEmotions = []
     for (let cat of cats){
         for(let emotion of cat.emotionTags){
-          
            if(!catEmotions.includes(emotion)){// use of both the .includes() operator and the logical not operator !
                catEmotions.push(emotion)
             } 
         }
     }
     return catEmotions
-} 
+}  
 
 function renderEmotionsRadios(cats){
     let radioItems = ` `
