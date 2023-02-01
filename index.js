9e7e8713f97091997349b874ed263f3504492d73
jr-philip <philipmwaura48@gmail.com>
2023-01-31 19:56:44 -0600

    done

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import { catsData } from "./data.js"
+
 const emotionRadios = document.getElementById("emotion-radios")
 const getImageBtn = document.getElementById("get-image-btn")
 const gifsOnlyOption = document.getElementById("gifs-only-option")
+const memeModalInner = document.getElementById("meme-modal-inner")
+const memeModal = document.getElementById("meme-modal")
+const memeModalCloseBtn = document.getElementById("meme-modal-close-btn")
 
 emotionRadios.addEventListener("change",hightLightCheckOption)// use of e.target.id 
 getImageBtn.addEventListener("click", renderCat)
+memeModalCloseBtn.addEventListener("click", closeModal)
+
+function closeModal(){
+    memeModal.style.display ="none"
+}
 
 
 function hightLightCheckOption(e){
@@ -47,7 +56,15 @@ function getSingleCatObject(){//then the renderCat() will call out these functio
 
 
 function renderCat(){// the eventListener will call these as the first function 
-    getSingleCatObject()
+    const catObject = getSingleCatObject()
+    memeModalInner.innerHTML = `
+        <img
+        class="cat-img"
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+        >
+    `
+memeModal.style.display ="flex"
 }
 
 
